04e3862ce1c9e8629ce5e0d14d9b20e06f8402b5
Tomas Hecko <tomas.hecko@fhb.sk>
2025-12-17 16:11:11 +0100

    plugin setting persistance

--- uninstall.php
+++ uninstall.php
@@ -4,14 +4,6 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 	exit;
 }
 
-global $wpdb;
-
-$plugin_options = $wpdb->get_results( "SELECT option_name FROM $wpdb->options WHERE option_name LIKE 'kika_%'" );
-
-foreach($plugin_options as $option) {
-	delete_option($option->option_name);
-}
-
 if (wp_next_scheduled('wp_job_fhb_kika_export_order')) {
     wp_clear_scheduled_hook('wp_job_fhb_kika_export_order');
 }
